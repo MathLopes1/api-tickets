9c85d7baf7442117ebf0307cc2d1bdaeb2823c44
MathLopes1 <iamatheuslopes@gmail.com>
2023-02-12 13:00:49 -0300

    feat: create controller and service ticket

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ const router = require('./routes/index.js');
 class App {
     constructor(){
         this.server = express();
+        this.middleware();
         this.routes();
     }
 
     routes() {
         router(this.server);
+    }
+
+    middleware() {
+        this.server.use(express.json())
     }
 }
 
