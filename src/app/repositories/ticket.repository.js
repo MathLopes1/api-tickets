487dc53cd32d436b04372e153dce4708206b62b3
MathLopes1 <iamatheuslopes@gmail.com>
2023-02-18 12:24:12 -0300

    feat: create services get

--- src/app/repositories/ticket.repository.js
+++ src/app/repositories/ticket.repository.js
@@ -2,10 +2,31 @@ const { PrismaClient } = require('@prisma/client')
 const prismaService = new PrismaClient()
 
 class TicketRepository {
-    create(ticket) {
-        return prismaService.tickets.create({
+    async create(ticket) {
+        return await prismaService.tickets.create({
             data: ticket
         });
+    }
+
+    async getAll() {
+        return await prismaService.tickets.findMany();
+    }
+
+    async getById(id) {
+        return await prismaService.tickets.findUnique({
+            where: {
+                id: id
+            }
+        })
+    }
+
+    async updateById(id, data) {
+        return await prismaService.tickets.update({
+            where: {
+                id: id
+            },
+            data: data
+        })
     }
 }
 
--- src/routes/features/ticket.router.js
+++ src/routes/features/ticket.router.js
@@ -2,6 +2,8 @@ const ticketController = require("../../app/controllers/ticket.controller");
 
 module.exports = (server, routes, prefix = '/api/v1/ticket') => {
     routes.post('/', ticketController.createTicket);
+    routes.get('/', ticketController.getAllTickets);
+    routes.get('/:id', ticketController.getTicketById);
     
     server.use(prefix, routes);
 };
