558db34bcf10461561c033064fd7a5f88f773d98
MathLopes1 <iamatheuslopes@gmail.com>
2023-02-18 23:02:56 -0300

    feat: adding cors for consumer api

--- src/routes/features/ticket.router.js
+++ src/routes/features/ticket.router.js
@@ -6,7 +6,7 @@ module.exports = (server, routes, prefix = '/api/v1/ticket') => {
     routes.get('/:id', ticketController.getTicketById);
     routes.put('/:id', ticketController.updateTicketById);
     routes.delete('/:id', ticketController.deleteTicketById);
-    
+
     server.use(prefix, routes);
 };
   
